569ef9cc43ee2c8d7a4c37237a0a02b897b458a7
zhichunl <liyaya95@gmail.com>
2014-10-15 20:31:04 -0400

    Created project

---
+++ MealPlan/MealPlan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MealPlan
+//
+//  Created by Zhichun Li on 10/15/14.
+//  Copyright (c) 2014 X2YZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
