57837e46e7c06295c557a1eb04e4c88ab9fb9f41
zhichunl <liyaya95@gmail.com>
2014-10-15 20:44:38 -0400

    Fixes cuz storyboard = nonono

--- MealPlan/MealPlan/AppDelegate.m
+++ MealPlan/MealPlan/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window.rootViewController = [[UIViewController alloc] init];
     return YES;
 }
 
