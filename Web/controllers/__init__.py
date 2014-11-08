23aec1c726f4946fa3ae7dfe0db4d41f414d6840
xiaote <xiaotez@andrew.cmu.edu>
2014-11-08 01:16:12 -0500

    revert some changes

--- Web/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-from pages import HomePage
-from pages import ProfilePage
-from pages import MenuPage
-from pages import OrderPage
--- Web/static/javascript/homepage.js
+++
@@ -1,8 +0,0 @@
-// This is some random code to test jquery
-$(document).ready(function() {
-
-    $(".woof").click(function () {
-        $("#birthdayImg").hide();
-        console.log("woof");
-    });
-});
